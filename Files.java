e16229520c6ab8afe7966b85ab30e3f3abf7149f
MOGUL1340 <antonioua1340@gmail.com>
2015-12-10 21:39:04 +0200

    Files.jpg Files.java

---
+++ Files.java
@@ -0,0 +1,17 @@
+public class File {
+
+}
+class AudioFile extends File {
+
+}
+class TextFile extends File {
+
+}
+class ImageFile extends File {
+
+}
+public class Directory {
+    File.AudioFile audiofile = new File.AudioFile();
+	File.TextFile textfile = new File.TextFileFile();
+	File.ImageFile imagefile = new File.ImageFile();
+}
