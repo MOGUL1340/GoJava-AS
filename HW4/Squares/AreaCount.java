21186c1af7f3efb765f8427df247ce1ba54062fa
MOGUL1340 <antonioua1340@gmail.com>
2015-12-22 10:47:14 +0200

    Update and rename SquaresCount.java to AreaCount.java

---
+++ HW4/Squares/AreaCount.java
@@ -0,0 +1,16 @@
+package HW4.HW4.Squares;
+
+public class AreaCount {
+    public static double getAreaCircle (double radius, double areaCircle) {
+        areaCircle = Math.PI * Math.pow(radius, 2);
+        return areaCircle;
+    }
+    public static double getAreaTriangle (int a = 2, double cathet1, double cathet2, double areaTriangle) {
+        areaTriangle = cathet1 * cathet2 / a;
+        return areaTriangle;
+    }
+    public static double getAreaRectangle (double side1, double side2, double areaRectangle) {
+        areaRectangle = side1 * side2;
+        return areaRectangle;
+    }
+}
