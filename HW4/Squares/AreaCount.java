02af1e4e9602acc4be61c50073f0e24f6a852322
MOGUL1340 <antonioua1340@gmail.com>
2015-12-22 16:43:51 +0200

    Update AreaCount.java

--- HW4/Squares/AreaCount.java
+++ HW4/Squares/AreaCount.java
@@ -1,17 +1,15 @@
 package HW4.HW4.Squares;
 
-import java.lang.Math;
-
 public class AreaCount {
-    public static double getAreaCircle (double radius, double areaCircle) {
+    public static double getAreaCircle (double radius) {
         areaCircle = Math.PI * Math.pow(radius, 2);
         return areaCircle;
     }
-    public static double getAreaTriangle (int a = 2, double cathet1, double cathet2, double areaTriangle) {
+    public static double getAreaTriangle (int a = 2, double cathet1, double cathet2) {
         areaTriangle = cathet1 * cathet2 / a;
         return areaTriangle;
     }
-    public static double getAreaRectangle (double side1, double side2, double areaRectangle) {
+    public static double getAreaRectangle (double side1, double side2) {
         areaRectangle = side1 * side2;
         return areaRectangle;
     }
