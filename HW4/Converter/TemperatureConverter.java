07c0cb896c9332dfff4662551d0cfb1f2313087b
MOGUL1340 <antonioua1340@gmail.com>
2015-12-22 10:39:33 +0200

    Update TemperatureConverter.java

--- HW4/Converter/TemperatureConverter.java
+++ HW4/Converter/TemperatureConverter.java
@@ -1,8 +1,6 @@
 package HW4.HW4.Converter;
 
 public class TemperatureConverter {
-    public double main(String[] args) {
-    }
     public static double getCelsiusToFarengheit(double cel) {
         return cel * 1.8 + 32;
     }
