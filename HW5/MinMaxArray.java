f07162e550b42bb1d1b6dc74bfd0723c4b14e2b7
MOGUL1340 <antonioua1340@gmail.com>
2015-12-29 16:56:06 +0200

    Update MinMaxArray.java

--- HW5/MinMaxArray.java
+++ HW5/MinMaxArray.java
@@ -1,40 +1,31 @@
 package HW5;
 
 import java.util.Arrays;
-import java.util.Random;
 
 public class MinMaxArray {
 
     public static void main(String[] args) {
-        int[] array = createArray();
+
+        int[] array = {1, 32, 5, 7, 28};
+        ArraySort sortArr = new ArraySort();
         System.out.println("Начальный массив");
         System.out.println(Arrays.toString(array));
         System.out.println("Массив сортированный методом вставки");
-        System.out.println(Arrays.toString(ArraySort.sortMyArray()));
+        System.out.println(Arrays.toString(ArraySort.sortMyArray(array)));
     }
 
-    public static int[] createArray() {
-        int[] massive = new int[5];
-        Random random = new Random();
-        for (int i = 0; i < massive.length; i++) {
-            massive[i] = random.nextInt(50);
-        }
-        return massive;
-    }
-
-
-    public static void minMaxArray () {
-        int[] minMax = createArray();
-        int minimum = minMax[0];
-        int maximum = minMax[0];
-        for (int i = 0; i < minMax.length; i++) {
-            if (minimum > minMax[i])
-                minimum = minMax[i];
+    public static void minMaxArray (int[] array) {
+    
+        int minimum = array[0];
+        int maximum = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (minimum > array[i])
+                minimum = array[i];
         }
 
-       for (int i = 0; i < minMax.length; i++) {
-            if (maximum < minMax[i])
-                maximum = minMax[i];
+        for (int i = 0; i < array.length; i++) {
+            if (maximum < array[i])
+                maximum = array[i];
         }
         System.out.println("Minimum = " + minimum);
         System.out.println("Maximum = " + maximum);
