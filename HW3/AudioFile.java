f183adbf9aafa54b9283a6d135e33fa50d49ebfd
MOGUL1340 <antonioua1340@gmail.com>
2016-01-13 23:06:44 +0200

    Обработка ошибок ДЗ6

--- HW3/AudioFile.java
+++ HW3/AudioFile.java
@@ -1,4 +1,11 @@
 package HW3;
 
 public class AudioFile extends File {
+    AudioFile audioFileValue;
+    try
+    {
+        audioFileValue = Directory.getFileValue();
+    } catch(IllegalArgumentException a){
+
+    }
 }
