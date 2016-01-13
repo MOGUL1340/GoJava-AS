da32bdd8f0192aba932370bb719193543d58a7d5
MOGUL1340 <antonioua1340@gmail.com>
2016-01-13 23:06:04 +0200

    Обработка ошибок ДЗ6

--- HW3/Directory.java
+++ HW3/Directory.java
@@ -1,5 +1,9 @@
 package HW3;
 
 public class Directory {
-
+    public static AudioFile getFileValue() {
+        AudioFile a = new AudioFile();
+        if(a = null) throw new IllegalArgumentException ("Audio File не должен быть пустым");
+        return a;
+    }
 }
