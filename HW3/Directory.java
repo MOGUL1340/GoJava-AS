341204cbb6174b100aa3ed6d444f4e7d46c5fcc3
MOGUL1340 <antonioua1340@gmail.com>
2016-01-15 17:42:01 +0200

    Update Directory.java

--- HW3/Directory.java
+++ HW3/Directory.java
@@ -3,7 +3,7 @@ package HW3;
 public class Directory {
     public static AudioFile getFileValue() {
         AudioFile a = new AudioFile();
-        if(a = null) throw new IllegalArgumentException ("Audio File не должен быть пустым");
+        if(a = null) throw new IllegalArgumentException ("Audio File must be not empty");
         return a;
     }
 }
