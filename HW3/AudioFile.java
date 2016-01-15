89e46bb586d830c37e1e08215c72aa7e4f05e77c
MOGUL1340 <antonioua1340@gmail.com>
2016-01-15 18:16:27 +0200

    Update AudioFile.java

--- HW3/AudioFile.java
+++ HW3/AudioFile.java
@@ -1,11 +1,5 @@
 package HW3;
 
 public class AudioFile extends File {
-    AudioFile audioFileValue;
-    try
-    {
-        audioFileValue = Directory.getFileValue();
-    } catch(IllegalArgumentException a){
-
-    }
+    
 }
