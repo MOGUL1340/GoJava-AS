6fd2445cd645c7d9037c436a2eb1872117761ee7
MOGUL1340 <antonioua1340@gmail.com>
2016-01-18 12:33:55 +0200

    Update SquaresCount.java

--- HW4/HW4/Squares/SquaresCount.java
+++ HW4/HW4/Squares/SquaresCount.java
@@ -1,10 +1,14 @@
 package HW4.HW4.Squares;
 
+/** Ниже в коде применяется паттерн Copy/Past, когда код просто копируется из одного места в другое и подставляются новые значения.
+ * Исправить это можно, применив Math, там где это необходимо, с соответствующими значениями.
+ * Этот код исправлен в окнчательной версии этой домашки */
+
 public class SquaresCount {
     public void getSquareCircle (String[] args) {
         double pi, radius, squareCircle;
-        pi = 3.14159;
-        squareCircle = pi * radius * radius;
+        pi = 3.14159; //наверное, это можно отнести к "меджик намберс" - вводится константа.
+        squareCircle = pi * radius * radius; //здесь нужно поменять, как написано в коментарии, в начале кода
         System.out.println(squareCircle);
     }
     public void getSquareTriangle (String[] args) {
