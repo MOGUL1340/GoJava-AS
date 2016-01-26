3172a9392b63f4c143502537f109b52e46281df0
MOGUL1340 <antonioua1340@gmail.com>
2016-01-26 16:07:34 +0200

    Update Directory.java

--- Files/Directory.java
+++ Files/Directory.java
@@ -1,5 +1,24 @@
 package HW3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Directory {
-    List <File> files;      //List потому, что нужно содержать дублирующиеся данные
-}
+
+    public static void addFile (String[] args) {
+
+      AudioFile audiofile = new AudioFile;
+      TextFile textfile = new TextFile;
+      ImageFile imagefile = new ImageFile;
+
+      List<String> fileslist = new ArrayList<>();
+
+      fileslist.add(audiofile);
+      fileslist.add(textfile);
+      fileslist.add(imagefile);
+
+      for (String str : fileslist) {
+        System.out.println(str);
+      }
+    }
+  }
