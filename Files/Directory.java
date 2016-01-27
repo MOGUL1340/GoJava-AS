20464503d16702541c6973886572a2aa4d9c26a0
MOGUL1340 <antonioua1340@gmail.com>
2016-01-28 01:00:58 +0200

    Update Directory.java

--- Files/Directory.java
+++ Files/Directory.java
@@ -6,16 +6,6 @@ import java.util.List;
 
 public class Directory {
 
-    public static void main(String[] args) {
-        AudioFile audiofile = new AudioFile("audio1",25);
-        TextFile textfile = new TextFile("text1", 1);
-        TextFile textfile2 = new TextFile("text2", 1);
-        ImageFile imagefile = new ImageFile("image1", 2);
-
-        
-
-    }
-
     List<File> fileslist = new ArrayList<>();
 
     public void addFile(File file) {
@@ -28,5 +18,19 @@ public class Directory {
         for (File file : fileslist) {
             System.out.println(file);
         }
+    }
+    public static void main(String[] args) {
+
+    Directory dir1 = new Directory();
+
+        AudioFile audiofile = new AudioFile("audio1",25);
+        TextFile textfile = new TextFile("text1", 1);
+        TextFile textfile2 = new TextFile("text2", 1);
+        ImageFile imagefile = new ImageFile("image1", 2);
+
+        dir1.addFile(audiofile);
+        dir1.addFile(textfile);
+        dir1.addFile(textfile2);
+        dir1.addFile(imagefile);
     }
 }
